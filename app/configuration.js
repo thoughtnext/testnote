332c06bbe3b981988658c8d9fac698fe7927f25b
Kritika <kritika@thoughtnext.com>
2016-12-06 14:48:15 +0530

    Updated configuration.js

--- app/configuration.js
+++ app/configuration.js
@@ -15,7 +15,7 @@ const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN)
 
 // URL where the app is running (include protocol). Used to point to scripts and 
 // assets located at this address. 
-const SERVER_URL = (process.env.SERVER_URL) ? (process.env.SERVER_URL)
+const SERVER_URL = (process.env.SERVER_URL)
 
 exports.APP_SECRET = APP_SECRET;
 exports.VALIDATION_TOKEN = VALIDATION_TOKEN;
